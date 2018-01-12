5519ca84f749977876d5931171d0fa3676ea05bc
Michael Jeffries <mjeff7@gmail.com>
2018-01-11 23:20:22 -0600

    Center component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,18 @@ import React from "react";
 import ResistorCalculator from "./components/ResistorCalculator";
 import "./App.css";
 
-const App = () => <ResistorCalculator />;
+const centeringStyle = {
+  display: "inline-block",
+  textAlign: "center",
+  border: "1px solid black",
+  margin: "1rem",
+  padding: "1rem"
+};
+
+const App = () => (
+  <div style={centeringStyle}>
+    <ResistorCalculator />
+  </div>
+);
 
 export default App;
