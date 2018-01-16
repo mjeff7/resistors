7b252ac0943b6e6fce70bca146ed37323fb6b6b2
Michael Jeffries <mjeff7@gmail.com>
2018-01-15 18:18:31 -0600

    Add and use skeleton of GenericBandSelector

--- src/components/ResistorCalculator.js
+++ src/components/ResistorCalculator.js
@@ -1,6 +1,12 @@
 // @flow
 
 import React from "react";
+import {
+  FIRST_DIGIT_COLORS,
+  SECOND_DIGIT_COLORS,
+  MULTIPLIER_COLORS,
+  TOLERANCE_COLORS
+} from "../calculator/colors";
 
 import type {
   FirstDigitColor,
@@ -14,33 +20,67 @@ type BandSelectorProps<T> = {
   value: T
 };
 
+type GenericBandSelectorProps<T> = BandSelectorProps<T> & {
+  values: Array<T>
+};
+
+const GenericBandSelector = <T>({
+  value,
+  values,
+  onSelectValue
+}: GenericBandSelectorProps<T>) => <div />;
+
 type FirstBandSelectorProps = BandSelectorProps<FirstDigitColor>;
 
 const FirstBandSelector = ({
   value,
   onSelectValue
-}: FirstBandSelectorProps) => <div />;
+}: FirstBandSelectorProps) => (
+  <GenericBandSelector
+    value={value}
+    onSelectValue={onSelectValue}
+    values={FIRST_DIGIT_COLORS}
+  />
+);
 
 type SecondBandSelectorProps = BandSelectorProps<SecondDigitColor>;
 
 const SecondBandSelector = ({
   value,
   onSelectValue
-}: SecondBandSelectorProps) => <div />;
+}: SecondBandSelectorProps) => (
+  <GenericBandSelector
+    value={value}
+    onSelectValue={onSelectValue}
+    values={SECOND_DIGIT_COLORS}
+  />
+);
 
 type MultiplierBandSelectorProps = BandSelectorProps<MultiplierColor>;
 
 const MultiplierBandSelector = ({
   value,
   onSelectValue
-}: MultiplierBandSelectorProps) => <div />;
+}: MultiplierBandSelectorProps) => (
+  <GenericBandSelector
+    value={value}
+    onSelectValue={onSelectValue}
+    values={MULTIPLIER_COLORS}
+  />
+);
 
 type ToleranceBandSelectorProps = BandSelectorProps<ToleranceColor>;
 
 const ToleranceBandSelector = ({
   value,
   onSelectValue
-}: ToleranceBandSelectorProps) => <div />;
+}: ToleranceBandSelectorProps) => (
+  <GenericBandSelector
+    value={value}
+    onSelectValue={onSelectValue}
+    values={TOLERANCE_COLORS}
+  />
+);
 
 export default () => (
   <div>
