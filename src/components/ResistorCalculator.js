2ee401af24fd7bf8dbcb6aeea9be839169fa16de
Michael Jeffries <mjeff7@gmail.com>
2018-01-15 18:03:44 -0600

    Add skeletons of selector menus

--- src/components/ResistorCalculator.js
+++ src/components/ResistorCalculator.js
@@ -2,4 +2,51 @@
 
 import React from "react";
 
-export default () => <div />;
+import type {
+  FirstDigitColor,
+  SecondDigitColor,
+  MultiplierColor,
+  ToleranceColor
+} from "../calculator/colors";
+
+type BandSelectorProps<T> = {
+  onSelectValue: T => void,
+  value: T
+};
+
+type FirstBandSelectorProps = BandSelectorProps<FirstDigitColor>;
+
+const FirstBandSelector = ({
+  value,
+  onSelectValue
+}: FirstBandSelectorProps) => <div />;
+
+type SecondBandSelectorProps = BandSelectorProps<SecondDigitColor>;
+
+const SecondBandSelector = ({
+  value,
+  onSelectValue
+}: SecondBandSelectorProps) => <div />;
+
+type MultiplierBandSelectorProps = BandSelectorProps<MultiplierColor>;
+
+const MultiplierBandSelector = ({
+  value,
+  onSelectValue
+}: MultiplierBandSelectorProps) => <div />;
+
+type ToleranceBandSelectorProps = BandSelectorProps<ToleranceColor>;
+
+const ToleranceBandSelector = ({
+  value,
+  onSelectValue
+}: ToleranceBandSelectorProps) => <div />;
+
+export default () => (
+  <div>
+    <FirstBandSelector value={"brown"} onSelectValue={() => {}} />
+    <SecondBandSelector value={"brown"} onSelectValue={() => {}} />
+    <MultiplierBandSelector value={"brown"} onSelectValue={() => {}} />
+    <ToleranceBandSelector value={"none"} onSelectValue={() => {}} />
+  </div>
+);
