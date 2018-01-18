312d2aa257c6cdd950419f246c55229d5f691791
Michael Jeffries <mjeff7@gmail.com>
2018-01-18 03:16:03 -0600

    Simplify function

--- src/utils.js
+++ src/utils.js
@@ -7,10 +7,9 @@ export const transformArgs = <T: $ReadOnlyArray<(*) => *>>(
   ...transformers: T
 ) => (toTransform: (...$TupleMap<T, ExtractReturnType>) => *) => (
   ...args: $TupleMap<T, ExtractInputType>
-) => {
-  const transformedArgs = transformers.map((transformer, argumentIndex) =>
-    transformer(args[argumentIndex])
+) =>
+  toTransform(
+    ...transformers.map((transformer, argumentIndex) =>
+      transformer(args[argumentIndex])
+    )
   );
-
-  return toTransform(...transformedArgs);
-};
