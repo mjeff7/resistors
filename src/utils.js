d8775876dc8119d88e6b048df98df07ed7fefce3
Michael Jeffries <mjeff7@gmail.com>
2018-01-18 03:14:07 -0600

    Enable flow and correct types

--- src/utils.js
+++ src/utils.js
@@ -1,6 +1,13 @@
-export const transformArgs = (...transformers: Array<(*) => *>) => (
-  toTransform: (*) => *
-) => (...args) => {
+// @flow
+
+type ExtractInputType = <T>((T) => *) => T;
+type ExtractReturnType = <T>((*) => T) => T;
+
+export const transformArgs = <T: $ReadOnlyArray<(*) => *>>(
+  ...transformers: T
+) => (toTransform: (...$TupleMap<T, ExtractReturnType>) => *) => (
+  ...args: $TupleMap<T, ExtractInputType>
+) => {
   const transformedArgs = transformers.map((transformer, argumentIndex) =>
     transformer(args[argumentIndex])
   );
