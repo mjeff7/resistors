24dbf73ac276116b05f11c448dc6bd4ed2aa4ac3
Michael Jeffries <mjeff7@gmail.com>
2018-01-18 02:29:38 -0600

    Add abbreviateValue

--- src/utils.js
+++ src/utils.js
@@ -13,3 +13,34 @@ export const transformArgs = <T: $ReadOnlyArray<(*) => *>>(
       transformer(args[argumentIndex])
     )
   );
+
+type NumberMagnitude = "m" | "" | "k" | "M" | "G";
+
+const MAGNITUDE_SUFFICES: { [*]: NumberMagnitude } = {
+  "-3": "m",
+  "0": "",
+  "3": "k",
+  "6": "M",
+  "9": "G"
+};
+
+/*
+ * abbreviateValue
+ *
+ * Take a real value between 1e-6 and 1e12 (exclusive) and returns a tuple
+ * [value, magnitude] where value is at least 1 and less than 1000 and magnitude
+ * is the correct suffix for the value. For example,
+ *
+ *  abbreviateValue(1234) === [1.234, 'k'] .
+ *
+ * It supports negative numbers as well for the same ranges.
+ */
+export const abbreviateValue = (value: number): [number, NumberMagnitude] => {
+  if (value === 0) return [0, ""];
+
+  const supportingE = Math.floor(Math.log10(Math.abs(value)) / 3) * 3;
+  const adjustedValue = value / Math.pow(10, supportingE);
+  const magnitudeSuffix = MAGNITUDE_SUFFICES[supportingE];
+
+  return [adjustedValue, magnitudeSuffix];
+};
--- src/utils.test.js
+++ src/utils.test.js
@@ -1,4 +1,4 @@
-import { transformArgs } from "./utils";
+import { abbreviateValue, transformArgs } from "./utils";
 
 describe("transformArgs", () => {
   it("transforms the arguments with the functions given", () => {
@@ -31,5 +31,67 @@ describe("transformArgs", () => {
     const wrapper = transformArgs(x => x, x => x)(target);
 
     expect(wrapper(0, "success: ")).toBe("correct");
+  });
+});
+
+describe("abbreviateValue", () => {
+  describe("returns a value at least 1 and less than 1000 or 0", () => {
+    const checkBounds = value => {
+      const result = Math.abs(abbreviateValue(value)[0]);
+      it(`${value}`, () => {
+        expect(result === 0 || (result >= 1 && result < 1000)).toBe(true);
+      });
+    };
+
+    checkBounds(0);
+    checkBounds(0.1);
+    checkBounds(-0.1);
+    checkBounds(0.0001);
+  });
+
+  describe("returns the same value for values at least 1 and less than 1000", () => {
+    const checkResults = value => {
+      const [result, suffix] = abbreviateValue(value);
+      it(`${value}`, () => {
+        expect(result).toBe(value);
+        expect(suffix).toBe("");
+      });
+    };
+
+    checkResults(0);
+    checkResults(999);
+    checkResults(999.9999);
+  });
+
+  describe("returns the same value for different orders of 1000", () => {
+    const checkResults = value => {
+      const [result1, suffix1] = abbreviateValue(value);
+      const [result2, suffix2] = abbreviateValue(value * 1000);
+      it(`${value} and ${value * 1000}`, () => {
+        expect(result1).toBeCloseTo(result2);
+        expect(suffix1).not.toBe(suffix2);
+      });
+    };
+
+    checkResults(1);
+    checkResults(999.9999);
+    checkResults(1000000);
+    checkResults(-0.0001);
+  });
+
+  describe("returns correct suffix", () => {
+    const checkResults = (value, correctSuffix) => {
+      const [result, suffix] = abbreviateValue(value);
+      it(`${value}`, () => {
+        expect(suffix).toBe(correctSuffix);
+      });
+    };
+
+    checkResults(0, "");
+    checkResults(1, "");
+    checkResults(1e-3, "m");
+    checkResults(1e3, "k");
+    checkResults(1e6, "M");
+    checkResults(1e9, "G");
   });
 });
