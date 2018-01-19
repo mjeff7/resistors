d4d191c20789047de28677d83ebe36969d019534
Michael Jeffries <mjeff7@gmail.com>
2018-01-18 18:07:15 -0600

    Split out method into own file

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,18 +1,6 @@
 // @flow
 
-type ExtractInputType = <T>((T) => *) => T;
-type ExtractReturnType = <T>((*) => T) => T;
-
-export const transformArgs = <T: $ReadOnlyArray<(*) => *>>(
-  ...transformers: T
-) => (toTransform: (...$TupleMap<T, ExtractReturnType>) => *) => (
-  ...args: $TupleMap<T, ExtractInputType>
-) =>
-  toTransform(
-    ...transformers.map((transformer, argumentIndex) =>
-      transformer(args[argumentIndex])
-    )
-  );
+export * from "./transformArgs";
 
 type NumberMagnitude = "m" | "" | "k" | "M" | "G";
 
--- src/utils/test.js
+++ src/utils/test.js
@@ -1,38 +1,4 @@
-import { abbreviateValue, transformArgs } from "./";
-
-describe("transformArgs", () => {
-  it("transforms the arguments with the functions given", () => {
-    const target = jest.fn();
-    const wrapper = transformArgs(x => x + 1)(target);
-
-    wrapper("hello");
-    expect(target).lastCalledWith("hello1");
-
-    wrapper(10);
-    expect(target).lastCalledWith(11);
-
-    expect(target).toHaveBeenCalledTimes(2);
-  });
-
-  it("transforms multiple arguments with the functions given", () => {
-    const target = jest.fn();
-    const wrapper = transformArgs(() => {}, () => {}, () => {}, x => x.length)(
-      target
-    );
-
-    wrapper(null, null, null, "hello");
-    expect(target).lastCalledWith(undefined, undefined, undefined, 5);
-
-    expect(target).toHaveBeenCalledTimes(1);
-  });
-
-  it("returns the value returned by the wrapped function", () => {
-    const target = jest.fn(() => "correct");
-    const wrapper = transformArgs(x => x, x => x)(target);
-
-    expect(wrapper(0, "success: ")).toBe("correct");
-  });
-});
+import { abbreviateValue } from "./";
 
 describe("abbreviateValue", () => {
   describe("returns a value at least 1 and less than 1000 or 0", () => {
