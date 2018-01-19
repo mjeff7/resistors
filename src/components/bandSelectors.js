2b0d96bb1824194da5c43a7e3ef37f51408f7a62
Michael Jeffries <mjeff7@gmail.com>
2018-01-19 05:52:07 -0600

    Add color to menus

--- src/components/bandSelectors.js
+++ src/components/bandSelectors.js
@@ -15,6 +15,50 @@ import {
 } from "../calculator/colors";
 import Select from "./Select";
 
+const MENU_BACKGROUND_COLORS = {
+  none: "white",
+  pink: "pink",
+  silver: "silver",
+  gold: "gold",
+  black: "black",
+  brown: "brown",
+  red: "red",
+  orange: "orange",
+  yellow: "yellow",
+  green: "green",
+  blue: "blue",
+  violet: "violet",
+  grey: "grey",
+  white: "white"
+};
+
+const MENU_TEXT_COLORS = {
+  none: "black",
+  pink: "black",
+  silver: "black",
+  gold: "black",
+  black: "white",
+  brown: "white",
+  red: "black",
+  orange: "black",
+  yellow: "black",
+  green: "white",
+  blue: "white",
+  violet: "black",
+  grey: "white",
+  white: "black"
+};
+
+const MENU_STYLES = Object.assign(
+  {},
+  ...Object.keys(MENU_BACKGROUND_COLORS).map(color => ({
+    [color]: {
+      color: MENU_TEXT_COLORS[color],
+      background: MENU_BACKGROUND_COLORS[color]
+    }
+  }))
+);
+
 type BandSelectorProps<T> = {
   onSelectValue: T => void,
   value: T
@@ -23,7 +67,7 @@ type BandSelectorProps<T> = {
 const GenericBandSelector = withProps(({ onSelectValue, values }) => ({
   onChange: onSelectValue,
   options: values,
-  styles: {}
+  styles: MENU_STYLES
 }))(Select);
 
 type BandSelector<T> = React.ComponentType<BandSelectorProps<T>>;
