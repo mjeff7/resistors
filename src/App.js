70d839678d8153270895bf0beb8e87c410fbd428
Michael Jeffries <mjeff7@gmail.com>
2018-01-19 16:23:21 -0600

    Move inline css into stylesheet

--- src/App.js
+++ src/App.js
@@ -4,16 +4,8 @@ import React from "react";
 import ResistorCalculator from "./components/ResistorCalculator";
 import "./App.css";
 
-const centeringStyle = {
-  display: "inline-block",
-  textAlign: "center",
-  border: "1px solid black",
-  margin: "1rem",
-  padding: "1rem"
-};
-
 const App = () => (
-  <div style={centeringStyle}>
+  <div>
     <div className="title">Resistance Calculator</div>
     <div className="subTitle">Turn color bands into resistor specs</div>
     <ResistorCalculator />
