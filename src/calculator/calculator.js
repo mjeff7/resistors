0b493f6d1f9ff50acd9f052eecee704ebc979a9e
Michael Jeffries <mjeff7@gmail.com>
2018-01-19 23:46:30 -0600

    Add comments and rearrange types

--- src/calculator/calculator.js
+++ src/calculator/calculator.js
@@ -1,5 +1,26 @@
 // @flow
 
+/*
+ * calculator.js
+ *
+ * This module provides the interface that translates a band of colors into
+ * numeric values and a resistance spec for a resisitor. For example:
+ *
+ *    calculateOhmValueFromColors(
+ *      Colors.RED,
+ *      Colors.BLUE,
+ *      Colors.BLACK,
+ *      Colors.NONE
+ *    ) === { resistance: 26, tolerance: 0.2 };
+ *
+ * This is separated into distinct stages:
+ *   1) Translate colors into numerical values (mostly handled by colors.js).
+ *   2) Combine those values into a resistance spec.
+ *   3) Optionally fill in the convenience values (minimum and maximum) of a
+ *      spec.
+ *
+ */
+
 import {
   type FirstDigit,
   type SecondDigit,
@@ -18,17 +39,16 @@ import type {
 } from "./colors";
 import { transformArgs } from "../utils";
 
-export const calculateOhmCenterValue = (
-  bandA: FirstDigit,
-  bandB: SecondDigit,
-  multiplierExponent: MultiplierExponent
-): number => (bandA * 10 + bandB) * Math.pow(10, multiplierExponent);
+/*
+ * Types
+ */
 
 export type CalculatedOhmValue = {
   resistance: number,
   tolerance: ToleranceValue
 };
 
+// A calculator with numerical parameters.
 export type OhmValueCalculator = (
   FirstDigit,
   SecondDigit,
@@ -36,6 +56,7 @@ export type OhmValueCalculator = (
   ToleranceValue
 ) => CalculatedOhmValue;
 
+// A calculator with color parameters.
 export type OhmValueCalculatorFromColors = (
   FirstDigitColor,
   SecondDigitColor,
@@ -43,6 +64,23 @@ export type OhmValueCalculatorFromColors = (
   ToleranceColor
 ) => CalculatedOhmValue;
 
+/*
+ * Combine numerical values into a resistance spec.
+ */
+
+// calculateOhmCenterValue
+//
+// Takes two digit bands and a multiplier and returns the resistance in ohms as
+// if the resistor were perfectly precise.
+export const calculateOhmCenterValue = (
+  bandA: FirstDigit,
+  bandB: SecondDigit,
+  multiplierExponent: MultiplierExponent
+): number => (bandA * 10 + bandB) * Math.pow(10, multiplierExponent);
+
+// calculateOhmValue
+//
+// Takes numerical values and returns the corresponding resistance spec.
 export const calculateOhmValue: OhmValueCalculator = (
   bandA,
   bandB,
@@ -53,6 +91,15 @@ export const calculateOhmValue: OhmValueCalculator = (
   tolerance
 });
 
+/*
+ * Optionally fill in the minimum and maximum of the spec.
+ */
+
+// attachToleranceBounds
+//
+// Takes a CalculatedOhmValue, e.g. as returned from calculateOhmValue, and
+// returns the same as a new object with the derived minimum and maximum values
+// included.
 export const attachToleranceBounds = ({
   resistance,
   tolerance
@@ -66,6 +113,10 @@ export const attachToleranceBounds = ({
   maximum: resistance * (1 + tolerance)
 });
 
+// wrapOhmValueCalculator
+//
+// Takes a calculator that operates on numerical values and returns a calculator
+// that does the same but operates on color bands instead.
 export const wrapOhmValueCalculator: OhmValueCalculator => OhmValueCalculatorFromColors = transformArgs(
   firstDigitFromColor,
   secondDigitFromColor,
@@ -73,6 +124,13 @@ export const wrapOhmValueCalculator: OhmValueCalculator => OhmValueCalculatorFro
   toleranceValueFromColor
 );
 
+/*
+ * Primary module export.
+ */
+
+// calculateOhmValueFromColors
+//
+// Take colors of bands and interprets them as a resistor spec.
 export const calculateOhmValueFromColors: (
   FirstDigitColor,
   SecondDigitColor,
