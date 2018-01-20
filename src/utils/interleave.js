500edf178eac0240abb457afa598300ffa1340fa
Michael Jeffries <mjeff7@gmail.com>
2018-01-20 02:09:54 -0600

    Add comments

--- src/utils/interleave.js
+++ src/utils/interleave.js
@@ -1,5 +1,14 @@
 // @flow
 
+/*
+ * interleave
+ *
+ * Interleaves two arrays and includes all elements in both arrays, even if
+ * one is shorter. For example,
+ *
+ *    interleave([0, 2, 4], [1, 3, 5, 7]) === [0, 1, 2, 3, 4, 5, 7]
+ */
+
 export const interleave = (arrA: Array<*>, arrB: Array<*>): Array<*> => {
   const maxLength = Math.max(arrA.length, arrB.length);
   const resultArray = [];
