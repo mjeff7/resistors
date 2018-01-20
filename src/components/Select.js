0105c0aa60738d62e0a84fcf7b99fe9dd892df69
Michael Jeffries <mjeff7@gmail.com>
2018-01-20 01:13:47 -0600

    Add comments

--- src/components/Select.js
+++ src/components/Select.js
@@ -1,11 +1,32 @@
 // @flow
 
+/*
+ * Select
+ *
+ * A managed component that wraps select and option DOM elements. It is typed so
+ * that the options given agree in type with the one chosen for display and the
+ * callback for when the user selects an option.
+ *
+ * Styles can be provided on the menu and the options.
+ */
+
 import React from "react";
 
 type Props<T> = {
+  // Callback called when the user selects an option.
   onChange: T => mixed,
+
+  // The options to present to the user.
   options: Array<T>,
+
+  // The currently selected value. Optional. Passed directly to the underlying
+  // DOM element.
   +value?: T,
+
+  // Styles for the component. Keys correspond to the values provided in
+  // options.  The values are objects passed as style objects to React. The
+  // value for special key null is passed as the style of the select element
+  // itself.
   +styles?: { [T | null]: ?{} }
 };
 
