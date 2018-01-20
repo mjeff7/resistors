36cdb00ac14a089febbc15ad29c6448c2d339707
Michael Jeffries <mjeff7@gmail.com>
2018-01-19 18:51:46 -0600

    Style title block

--- src/App.js
+++ src/App.js
@@ -6,8 +6,10 @@ import "./App.css";
 
 const App = () => (
   <div>
-    <div className="title">Resistance Calculator</div>
-    <div className="subTitle">Turn color bands into resistor specs</div>
+    <div className="titleBlock">
+      <div className="title">Resistance Calculator</div>
+      <div className="subTitle">Turn color bands into resistor specs</div>
+    </div>
     <ResistorCalculator />
   </div>
 );
