ba9b049d4c5a7f635fa188c9c3a6ad57f5609588
Michael Jeffries <mjeff7@gmail.com>
2018-01-20 22:31:51 -0600

    Record RAL color specs

--- src/components/bandSelectors.js
+++ src/components/bandSelectors.js
@@ -28,6 +28,26 @@ import Select from "./Select";
  * Styling.
  */
 
+// Taken from the RAL spec as specified at
+// https://en.wikipedia.org/wiki/Electronic_color_code
+// and converted to hex http://rgb.to/ral/ .
+const RAL_COLORS = {
+  pink: "#d8a0a6",
+  // These colors have no RAL code.
+  // silver: '',
+  // gold: '',
+  black: "#0e0e10",
+  brown: "#7e4b26",
+  red: "#a72920",
+  orange: "#f67828",
+  yellow: "#f6b600",
+  green: "#61993b",
+  blue: "#007cb0",
+  violet: "#76689a",
+  grey: "#7a888e",
+  white: "#e3d9c6"
+};
+
 // Background display color for each color.
 const MENU_BACKGROUND_COLORS = {
   none: "white",
